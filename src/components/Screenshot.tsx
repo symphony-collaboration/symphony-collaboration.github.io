d32dff9f544ae49fe054ee30e4e450b2cf4dd0d7
Yusuf Birader <yusuf.birader@hotmail.com>
2023-09-21 10:26:11 +0100

    crop images

--- src/components/Screenshot.tsx
+++ src/components/Screenshot.tsx
@@ -4,7 +4,7 @@ export default function Screenshot() {
   return (
     <div className="relative flex items-center justify-center px-4 sm:px-6">
       <div className="border-1 z-10 flex justify-center rounded-2xl border-[0.5px] border-solid border-stone-200/60 bg-white/20 p-2 shadow backdrop-blur-md dark:shadow-lg dark:shadow-stone-400/30 sm:rounded-[32px] sm:p-5 md:rounded-[44px] md:p-7">
-        <video autoPlay loop className="z-20 w-full max-w-[1200px]">
+        <video autoPlay={true} loop={true} muted={true} playsInline={true} className="z-20 w-full max-w-[1200px]">
           <source src='/img/hey.mp4' type='video/mp4'></source>
         </video>
       </div>
